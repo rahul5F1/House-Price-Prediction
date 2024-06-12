67031cb9a32a3cbe404b55a4f18c46a440757cad
Ramavath Rahul <21311a05f1@sreenidhi.edu.in>
2024-06-12 22:50:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,12 @@ def predict():
     return str(prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080, use_reloader=False)
+    app.run(debug=True, port=5000)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+
 
 
 
